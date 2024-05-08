c978584e1ecdb3ddc3e9b5d89fb76d50bec3738f
Jasverenne Ferrer <jasverennepferr@gmail.com>
2024-05-08 20:22:10 +0800

    fix routing and created folders for different webpages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import Home from "./pages/Home";
+import Home from "./pages/HomePage/Home";
+import EmployeePage from './pages/EmployeePage/Bank-Employee';
+import ClientPage from './pages/ClientPage/ClientPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+             <Route index element={<Home></Home>}></Route>
+             <Route path='employee' element={<EmployeePage></EmployeePage>}></Route>
+             <Route path='client' element={<ClientPage></ClientPage>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
