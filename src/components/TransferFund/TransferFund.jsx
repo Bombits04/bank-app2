9c3cbf80ff191dd52dfd50af786684cf42139dff
Ian Hidalgo <bombits.03@gmail.com>
2024-05-10 21:28:12 +0800

    added styles and input boxes to modal components

--- src/components/TransferFund/TransferFund.jsx
+++ src/components/TransferFund/TransferFund.jsx
@@ -1,24 +1,37 @@
 //CSS IS IN WithdrawFunds.css
 import ReactDom from "react-dom";
 
-function TransferFund({transOpen, transClose}){
-    if (!transOpen) return null
-    return ReactDom.createPortal(
-        <div className="modal-container">
-          <div className="modal-comp-container">
-            <div onClick={transClose} className="modal-exit-button">
-              [X]
+function TransferFund({ transOpen, transClose }) {
+  if (!transOpen) return null;
+  return ReactDom.createPortal(
+    <div className="modal-container">
+      <div className="modal-comp-container">
+        <div onClick={transClose} className="modal-exit-button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="25px" height="20px"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+        </div>
+        <div className="modal-item-container">
+          <div className="modal-title"><span>Transfer Funds</span></div>
+          <div className="modal-body">
+            <div className="input-row">
+              <span>From acct no.: </span>
+              <input type="number" />
             </div>
-            <div className="modal-item-container">
-              <div className="modal-title">Transfer Funds</div>
-              <div className="modal-body">MODAL BODY</div>
-              <div className="modal-footer">MODAL FOOTER</div>
+            <div className="input-row">
+              <span>To acct no.: </span>
+              <input type="number" min="1" />
+            </div>
+            <div className="input-row">
+              <span>Amount: </span>
+              <input type="number" min={0} minLength={5} maxLength={5} />
             </div>
           </div>
-        </div>,
-    
-        document.getElementById("portal")
-    );
+          <div className="modal-footer">MODAL FOOTER</div>
+        </div>
+      </div>
+    </div>,
+
+    document.getElementById("portal")
+  );
 }
 
 export default TransferFund;
--- src/components/WithdrawFund/WithdrawFund.jsx
+++ src/components/WithdrawFund/WithdrawFund.jsx
@@ -1,5 +1,5 @@
 import "./WithdrawFund.css";
-import withdrawImg from "../../../src/assets/images/withdraw.png";
+
 import ReactDom from "react-dom";
 
 function WithdrawFund({ withOpen, withClose }) {
@@ -9,12 +9,35 @@ function WithdrawFund({ withOpen, withClose }) {
     <div className="modal-container">
       <div className="modal-comp-container">
         <div onClick={withClose} className="modal-exit-button">
-          [X]
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            width="25px"
+            height="20px"
+          >
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
         </div>
         <div className="modal-item-container">
-          <div className="modal-title">Withdraw Funds</div>
-          <div className="modal-body">MODAL BODY</div>
-          <div className="modal-footer">MODAL FOOTER</div>
+          <div className="modal-title">
+            <span>Withdraw Funds</span>
+          </div>
+          <div className="modal-body">
+            <div className="input-row">
+              <span>From acct no.: </span>
+              <input type="number" />
+            </div>
+            <div className="input-row">
+              <span>Amount: </span>
+              <input type="number"/>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <div className="button-row">
+              <button>ok</button>
+              <button>Cancel</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>,
